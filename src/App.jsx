a35faf3ea94e8c7520a15814626b12d910a64499
Lucas <lucassantosamaral@hotmail.com>
2019-03-18 21:19:18 -0300

    Adicionando background da página

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './scss/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>teste</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
