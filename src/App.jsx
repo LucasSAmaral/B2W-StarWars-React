d38e4fd17fe430781378951bb964992dd228bd0f
Lucas <lucassantosamaral@hotmail.com>
2019-03-22 22:26:29 -0300

    Renderizando intro somente nas resolucoes acima de 1024

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,26 @@ import ALongTimeAgo from './components/ALongTimeAgo';
 import './scss/Style.scss';
 
 class App extends Component {
+
+  renderIntro(){
+    let screenWidth = window.innerWidth;
+    if(screenWidth >= 1024){
+      return(
+        <div>
+          <ThemeMusic />
+          <ALongTimeAgo />
+        </div>
+      )
+    }
+  }
+  
   render() {
     return (
       <div className="App">
         <div className="container">
             <ImageLogo />
             <MenuButtons />
-            <ThemeMusic />
-            <ALongTimeAgo />
+            {this.renderIntro()}
         </div>
       </div>
     );
