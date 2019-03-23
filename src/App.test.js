88588819fe714cf13c93cd64add7ea1c4a551336
Lucas <lucassantosamaral@hotmail.com>
2019-03-22 22:59:16 -0300

    Adicionando teste

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
+import AboutMe from './pages/AboutMe';
+import AboutTheApp from './pages/AboutTheApp';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders the About Me page main title', ()=>{
+  const wrapper = shallow(<AboutMe />);
+  const title = <h1 className="about__title">About Me</h1>;
+  expect(wrapper.contains(title)).toEqual(true);
+});
+
+it('renders the About The App page main title', ()=>{
+  const wrapper = shallow(<AboutTheApp />);
+  const title = <h1 className="about__title">About The App</h1>;
+  expect(wrapper.contains(title)).toEqual(true);
 });
